ec8af4c2f1c1e5cde1db9a0ef9ab368ff069f0b9
Alisha <alisha-yasmin.aydin@gmx.ch>
2022-03-14 10:09:22 +0100

    hi

--- python_scripts/data_processing.py
+++ python_scripts/data_processing.py
@@ -2,7 +2,7 @@
 import pandas as pd
 years = [f'{i}' for i in range(2007, 2022)]
 
-
+#test alisha
 def get_df_of_year(year):
     df_air = pd.read_csv(f'../data/air_quality/ugz_ogd_air_h1_{year}.csv')
     df_traffic = pd.read_csv(f'../data/traffic/ugz_ogd_traffic_h1_{year}.csv')
