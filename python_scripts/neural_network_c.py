3f8d86e490bc4c40f3647bfdfef9a1bf0a3ec1c9
lfranceschetti <lfranceschetti@gmx.ch>
2022-05-14 15:45:11 +0200

    Neural network updated

--- python_scripts/neural_network_c.py
+++ python_scripts/neural_network_c.py
@@ -1,4 +1,5 @@
 # --- installing all packages: ---
+from cProfile import label
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
@@ -13,21 +14,38 @@ from keras.models import Model
 from keras.layers import Input, Dense
 from sklearn import metrics
 
+###################################
+#        Model configuaration     #
+###################################
 
-#!
-n = 2500
-data = pd.read_csv('../processed_data/full_data_imputed_with_EAQI.csv')
-data = data[0:n]
+data = pd.read_csv('../processed_data/full_data_imputed.csv')
+
+hist_window = 48
+horizon = 48
+TRAIN_SPLIT = int(0.7 * data.shape[0])
+batch_size = 5
+buffer_size = 150
+num_epochs = 5
+steps_per_epoch = None
+
+gases = ['NO2', 'NO','NOx', 'O3', 'PM10', 'SO2', 'CO']
+x_labels = ['Zweirad', 'Personenwagen', 'Lastwagen', 'Hr', 'RainDur', 'T', 'WVs', 'StrGlo', 'p'] 
+gas = "O3"
+
+###################################
+#        Data preprocessing       #
+###################################
+
+x_labels.append(gas)
+
 for i in data.select_dtypes('object').columns:
     le = LabelEncoder().fit(data[i])
     data[i] = le.transform(data[i])
 
 X_scaler = MinMaxScaler()
 Y_scaler = MinMaxScaler()
-X_data = X_scaler.fit_transform(
-    data[['Datum', 'Zweirad', 'Personenwagen', 'Lastwagen', 'Hr', 'RainDur', 'T', 'WVs', 'StrGlo', 'p', 'NO2', 'NO',
-          'NOx', 'O3', 'PM10', 'SO2', 'CO']])
-Y_data = Y_scaler.fit_transform(data[['CO']])
+X_data = X_scaler.fit_transform(data[x_labels])
+Y_data = Y_scaler.fit_transform(data[[gas]])
 
 
 def custom_ts_multi_data_prep(dataset, target, start, end, window, horizon):
@@ -43,25 +61,27 @@ def custom_ts_multi_data_prep(dataset, target, start, end, window, horizon):
         y.append(target[indicey])
     return np.array(X), np.array(y)
 
-
-hist_window = 200 # TODO wechseln wie unten (1)
-horizon = 800
-TRAIN_SPLIT = 500
 x_train, y_train = custom_ts_multi_data_prep(X_data, Y_data, 0, TRAIN_SPLIT, hist_window, horizon)
 x_vali, y_vali = custom_ts_multi_data_prep(X_data, Y_data, TRAIN_SPLIT, None, hist_window, horizon)
 
-
-batch_size = 256
-buffer_size = 150
+print(x_train.shape)
+print(y_train.shape)
+print(x_vali.shape)
+print(y_vali.shape)
 
 train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-train_data = train_data.cache().shuffle(buffer_size).batch(batch_size).repeat()
+train_data = train_data.cache().shuffle(buffer_size).batch(batch_size)
 val_data = tf.data.Dataset.from_tensor_slices((x_vali, y_vali))
-val_data = val_data.batch(batch_size).repeat()
+val_data = val_data.batch(batch_size)
+validate = data[x_labels].tail(hist_window)
+data.drop(data.tail(horizon).index,inplace=True)
 
-validate = data[['Datum', 'Zweirad', 'Personenwagen', 'Lastwagen', 'Hr', 'RainDur', 'T', 'WVs', 'StrGlo', 'p', 'NO2', 'NO',
-          'NOx', 'O3', 'PM10', 'SO2', 'CO']].tail(200) # TODO wechseln wie oben (1)
-data.drop(data.tail(10).index,inplace=True)
+
+
+###################################
+#              Model              #
+###################################
+
 
 lstm_model = tf.keras.models.Sequential([
     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(200, return_sequences=True),
@@ -83,8 +103,8 @@ checkpoint = tf.keras.callbacks.ModelCheckpoint(model_path, monitor='val_loss', 
                                                 verbose=0)
 callbacks = [early_stopings, checkpoint]
 
-history = lstm_model.fit(train_data, epochs=5, steps_per_epoch=5, validation_data=val_data, validation_steps=50,
-                         verbose=1, callbacks=callbacks)
+history = lstm_model.fit(train_data, epochs=num_epochs, steps_per_epoch=steps_per_epoch, validation_data=val_data, validation_steps=50,
+                        verbose=1, callbacks=callbacks)
 
 plt.figure(figsize=(16, 9))
 plt.plot(history.history['loss'])
@@ -95,15 +115,19 @@ plt.xlabel('epoch')
 plt.legend(['train loss', 'validation loss'])
 plt.show()
 
-data_val = X_scaler.fit_transform(
-    data[['Datum', 'Zweirad', 'Personenwagen', 'Lastwagen', 'Hr', 'RainDur', 'T', 'WVs', 'StrGlo', 'p', 'NO2', 'NO',
-          'NOx', 'O3', 'PM10', 'SO2','CO' ]].tail(200))  # TODO wechseln wie oben (1)
+print(history)
+#lstm_model = keras.models.load_model('Bidirectional_LSTM_Multivariate.h5')
+
+#pred = lstm_model.predict(train_data)
+
+
+data_val = X_scaler.fit_transform(data[x_labels].tail(horizon))  
 
 
 val_rescaled = data_val.reshape(1, data_val.shape[0], data_val.shape[1])
 pred = lstm_model.predict(val_rescaled)
 pred_Inverse = Y_scaler.inverse_transform(pred)
-pred_Inverse
+
 
 
 def timeseries_evaluation_metrics_func(y_true, y_pred):
@@ -119,12 +143,12 @@ def timeseries_evaluation_metrics_func(y_true, y_pred):
     print(f'R2 is : {metrics.r2_score(y_true, y_pred)}', end='\n\n')
 
 
-timeseries_evaluation_metrics_func(validate['CO'], pred_Inverse[0])  #TODO ich glaube das sollte jetzt gehen
+timeseries_evaluation_metrics_func(validate[gas], pred_Inverse[0]) 
 
 plt.figure(figsize=(16, 9))
-plt.plot(list(pred_Inverse[0]))
-plt.plot( list(validate['CO']))
+plt.plot(list(pred_Inverse[0]), color="red")
+plt.plot(list(validate[gas]), color="black")
 plt.title("Actual vs Predicted")
 plt.ylabel("CO")
-plt.legend(('Actual', 'predicted'))
+plt.legend(('predicted','Actual'))
 plt.show()
